b96d596424e09ff39c94ca7ff60ddbc5b0fe1a61
joethorley <joe@poissonconsulting.ca>
2019-06-20 13:47:38 -0700

    rm mcmc_derive.mcmc_data

--- R/namespace.R
+++ R/namespace.R
@@ -1,3 +1,2 @@
 #' @import err checkr mcmcr
-#' @importFrom mcmcdata mcmc_data
 NULL
--- tests/testthat/test-derive.R
+++ tests/testthat/test-derive.R
@@ -64,15 +64,3 @@ test_that("derive matrix", {
   expect_identical(derived, mcmcr)
 })
 
-test_that("derive.mcmc_data", {
-  
-  mcmc_data <- mcmcdata::mcmc_data_example
-  mcmc_data$data$count <- 1:2
-  derived <- mcmc_derive(mcmc_data, expr = "prediction <- estimate * count")
-  expect_is(derived, "mcmc_data")
-  
-  mcmc_data <- coef(mcmc_data)
-  derived <- coef(derived)
-  
-  expect_identical(mcmc_data$estimate * 1:2, derived$estimate)
-})
