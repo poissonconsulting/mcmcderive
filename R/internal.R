b0be7089bfbcc0399e911d050c8e3568c4d14444
joethorley <joe@poissonconsulting.ca>
2019-06-25 09:19:16 -0700

    remove unused code

--- R/internal.R
+++ R/internal.R
@@ -1,4 +1,3 @@
-
 variables <- function(expr) {
   all.vars(parse(text = expr))
 }
@@ -11,22 +10,3 @@ set_class <- function(x, class) {
   class(x) <- class
   x
 }
-
-derive2 <- function(object, expr, values, monitor) {
-  
-  object <- lapply(object, as.mcarray)
-  object <- lapply(object, unclass)
-  values <- c(object, values)
-  print(values)
-  print(expr)
-  object <- within(values, eval(parse(text = expr)))
-  print(object)
-  object <- object[monitor]
-  
-  # missing values
-  object <- lapply(object, set_class, "mcmcarray")
-  object <- as.mcmcr(object)
-  check_mcmcr(object)
-  object
-}
-
