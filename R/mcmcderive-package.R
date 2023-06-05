5c1c00a1613f3ed9f58480499fb42165b4c01318
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-05 19:14:16 +0200

    Document

--- R/mcmcderive-package.R
+++ R/mcmcderive-package.R
@@ -2,7 +2,13 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom rlang call2
 #' @importFrom rlang enexpr
+#' @importFrom rlang enquo
+#' @importFrom rlang eval_tidy
 #' @importFrom rlang is_string
+#' @importFrom rlang parse_exprs
+#' @importFrom rlang quo_get_expr
+#' @importFrom rlang quo_is_null
 ## usethis namespace: end
 NULL
